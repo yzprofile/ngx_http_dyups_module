e75e321e0f97d15d7db093de59bbc13d9f2e8f7a
yzprofile <yzprofiles@gmail.com>
2013-04-26 15:12:06 +0800

    Fixed stupid bug

--- ngx_http_dyups_module.c
+++ ngx_http_dyups_module.c
@@ -497,7 +497,7 @@ ngx_http_dyups_init_process(ngx_cycle_t *cycle)
     dmcf = ngx_http_cycle_get_module_main_conf(ngx_cycle,
                                                ngx_http_dyups_module);
 
-    if (!dmcf || dmcf->enable) {
+    if (!dmcf || !dmcf->enable) {
         return NGX_OK;
     }
 
